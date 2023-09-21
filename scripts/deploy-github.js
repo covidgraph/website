63916e82fea87f1f86ed804f75a74a6bf1831542
Jiros <jamie@munro.consulting>
2023-09-21 21:14:21 +0200

    add github pages deploy scripts

---
+++ scripts/deploy-github.js
@@ -0,0 +1,13 @@
+const ghpages = require('gh-pages')
+
+// replace with your repo url
+ghpages.publish(
+  'public',
+  {
+    branch: 'main',
+    repo: 'https://github.com/covidgraph/website',
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
